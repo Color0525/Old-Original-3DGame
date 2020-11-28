9404d05e891c838ec2a0f74a41989688baf377ee
Color0525 <rio.game.kojima@gmail.com>
2020-11-28 17:10:39 +0900

    First Commit

---
+++ Assets/Scripts/EnemyGenerater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerater : MonoBehaviour
+{
+    [SerializeField] GameObject m_mapEnemyPrefabs;
+    [SerializeField] int m_instanceNum = 3;
+    [SerializeField] float m_generateRange = 20f;
+    [SerializeField] float m_playerDis = 10f;
+
+    public void GenerateEnemy()
+    {
+        Vector3 generaterPos = transform.position;
+        for (int i = 0; i < m_instanceNum; i++)
+        {        
+            float randomX = Random.Range(generaterPos.x - m_generateRange, generaterPos.x + m_generateRange);
+            float randomZ = Random.Range(generaterPos.z - m_generateRange, generaterPos.z + m_generateRange);
+            Vector3 instancePos = new Vector3(randomX, generaterPos.y, randomZ);
+            Vector3 playerPos = FindObjectOfType<HumanoidController>().transform.position;
+            if (Vector3.Distance(instancePos, playerPos) > m_playerDis)
+            {
+                Instantiate(m_mapEnemyPrefabs, instancePos, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController m_Instance { get; private set; }
+
+    public GameObject[] m_playerPrefabs;
+    public GameObject[] m_enemyPrefabs;
+
+    public Vector3 m_playerMapPosition = Vector3.zero;
+    public Quaternion m_playerMapRotation = Quaternion.identity;
+
+    void Awake()
+    {
+        if (m_Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            m_Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void LoadBattleScene(GameObject[] playerPrefabs, GameObject[] enemyPrefabs, Vector3 playerMapPos, Quaternion playerMapRotate)
+    {
+        foreach (var player in FindObjectsOfType<HumanoidController>())
+        {
+            player.StopControl();
+        }
+        foreach (var enemy in FindObjectsOfType<MapEnemyController>())
+        {
+            enemy.StopControl();
+        }
+
+        m_playerPrefabs = playerPrefabs;
+        m_enemyPrefabs = enemyPrefabs;
+        m_playerMapPosition = playerMapPos;
+        m_playerMapRotation = playerMapRotate;
+
+        SceneManager.LoadScene("Battle");
+        //StartCoroutine(LoadScene("Battle"));
+    }
+
+    public void LoadMapScene()
+    {
+        SceneManager.LoadScene("Map");
+        //StartCoroutine(LoadScene("Map"));
+    }
+
+    IEnumerator LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+        yield return null;
+    }
+}

72879767af6c7e4a6b7764e4136ce411bded3141
Color0525 <rio.game.kojima@gmail.com>
2020-11-28 17:09:29 +0900

    Deleted

--- Assets/Scripts/MapPlayerInformation.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MapPlayerInformation : MonoBehaviour
-{
-    public GameObject[] m_playerPrefabs;
-}

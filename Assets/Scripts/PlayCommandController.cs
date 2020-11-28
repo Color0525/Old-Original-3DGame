3206c91ad768953e6e2140255a3a8d93b0ba1879
Color0525 <rio.game.kojima@gmail.com>
2020-11-28 17:44:11 +0900

    First commit

---
+++ Assets/Scripts/PlayCommandController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCommandController : MonoBehaviour
+{
+    public BattlePlayerController m_bpc; 
+
+    public void PlayCommand()
+    {
+        m_bpc.AttackCommand();
+    }
+
+}

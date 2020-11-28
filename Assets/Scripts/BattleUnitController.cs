14ce96b122403b01a3acc0d919e9b5a43e209775
Color0525 <rio.game.kojima@gmail.com>
2020-11-28 16:28:54 +0900

    Original 3DGame Project
    First Commit

---
+++ Assets/Scripts/BattleUnitController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleUnitController : MonoBehaviour
+{
+    public virtual void StartAction() 
+    {
+        FindObjectOfType<BattleManager>().StartActingTurn();
+    }
+
+    public virtual void EndAction()
+    {
+        FindObjectOfType<BattleManager>().EndActingTurn();
+    }
+}
---
+++ Assets/Scripts/MapEnemyInformation.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEnemyInformation : MonoBehaviour
+{
+    public GameObject[] m_enemyPrefabs;
+}
